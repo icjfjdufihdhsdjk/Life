9cfafd333ca3ec5bdaea0c414c31e1b75cfaad43
icjfjdufihdhsdjk <96685566+icjfjdufihdhsdjk@users.noreply.github.com>
2026-08-16 17:01:47 +1100

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const gate = document.getElementById("gate");
 
+let opened = false;
+
+gate.addEventListener("click", () => {
+
+    if (opened) return;
+
+    opened = true;
+
+    gate.classList.add("open");
+    document.body.classList.add("open");
+
+});
